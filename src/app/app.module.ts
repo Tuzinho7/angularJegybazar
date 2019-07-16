b041f44c033c458255f4bd7d416eb75c998ca4fd
Tuza Gábor <Tuza.Gabor@euroone.hu>
2019-07-16 08:59:54 +0200

    jegybazar refaktor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertModule, CollapseModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    FooterComponent,
+    EventcardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
